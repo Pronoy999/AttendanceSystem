b6154035cff3dad0d42224f7ef1e0d08a45a0a67
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-10-24 10:49:35 +0530

    Fixed the bug where the connection was getting timed out.

--- databaseHandler.js
+++ databaseHandler.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 /**
  * AWS Database credentials.
  */
-var pool = mysql.createPool({
+const pool = mysql.createPool({
     host: 'hx-db.coh0oaulbzdr.ap-southeast-1.rds.amazonaws.com',
     user: 'db_admin',
-    database: 'staging_diagnostic_app',
+    database: 'diagnostic_app',
     password: 'hxadmin123',
     port: '3306'
 });
@@ -45,12 +45,15 @@ database.insert = function (tableName, values, callback) {
  * @param callback: The Method callback.
  */
 database.query = function (queryStatement, callback) {
+    //console.log('In Query.');
     pool.getConnection(function (err, con) {
         if (err) {
             callback(err);
             console.log(err);
         } else {
+            //console.log('Got Connection');
             con.query(queryStatement, function (err, results, fields) {
+                con.release();
                 if (err) {
                     callback(err);
                     console.log(err);
@@ -60,10 +63,10 @@ database.query = function (queryStatement, callback) {
             });
         }
         pool.on('release', function (con) {
-            //Connection released.
+            //Released.
         });
         pool.on('acquire', function (con) {
-            //Connection Acquired.
+            //Acquired.
         });
     });
 };
@@ -84,6 +87,7 @@ database.update = function (tableName, updateCol, updateVal, where, callback) {
             var queryStatement = "UPDATE " + tableName + " SET " + updateCol + " = " +
                 updateVal + " WHERE " + where;
             con.query(queryStatement, function (err, results, fields) {
+                con.release();
                 if (err) {
                     callback(err, {});
                     console.log(err);
@@ -92,6 +96,12 @@ database.update = function (tableName, updateCol, updateVal, where, callback) {
                 }
             });
         }
+        pool.on('release', function (con) {
+            //Connection released.
+        });
+        pool.on('acquire', function (con) {
+            //Connection Acquired.
+        });
     });
 };
 module.exports = database;
