e4df23982b351d4776572ab1331596b6985d50ab
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-11-07 01:07:54 +0530

    Changed the SMS Message.

--- Constants.js
+++ Constants.js
@@ -7,7 +7,7 @@ messages.companyPrefix = "HX";
 messages.phoneInserted = "Inserted new Phone.";
 messages.insufficientData = "Insufficient Data.";
 messages.dateFormat = 'YYYY-MM-DD HH:mm:ss';
-messages.sellPhoneMessage = 'We have received your order for sell your phone and pick up will be scheduled ASAP.';
+messages.sellPhoneMessage = 'thank you for showing interest to sell your phone at the best price. A HyperXpert would connect with you shortly.';
 /**
  * Exporting the Message Module.
  */
