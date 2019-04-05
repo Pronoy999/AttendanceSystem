58f356e8ef2ae36431bb1b82934c5ae55ebcc4e1
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2019-04-05 13:16:23 +0530

    Added POS order sell and Security Check.

--- snsLib.js
+++ snsLib.js
@@ -1,9 +1,9 @@
 const aws = require('aws-sdk');
 var sms = {};
 aws.config.update({
-    region: 'ap-southeast-1',
-    accessKeyId: 'AKIAJZLKRHVB7IYQIFWA',
-    secretAccessKey: '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'
+   region: 'ap-southeast-1',
+   accessKeyId: 'AKIAJZLKRHVB7IYQIFWA',
+   secretAccessKey: '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'
 });
 var sns = new aws.SNS();
 /**
@@ -12,17 +12,17 @@ var sns = new aws.SNS();
  * @param callback: The Method callback. The error and the OTP.
  */
 sms.sendOTP = function (phone, callback) {
-    const helpers = require('./helpers');
-    var number = helpers.createOTP();
-    var msg = 'Your HX OTP is: ' + number;
-    var params = {
-        Message: msg,
-        MessageStructure: 'string',
-        PhoneNumber: phone
-    };
-    sns.publish(params, function (err, data) {
-        callback(err, number);
-    });
+   const helpers = require('./helpers');
+   var number = helpers.createOTP();
+   var msg = 'Your HX OTP is: ' + number;
+   var params = {
+      Message: msg,
+      MessageStructure: 'string',
+      PhoneNumber: phone
+   };
+   sns.publish(params, function (err, data) {
+      callback(err, number);
+   });
 };
 /**
  * Method to send normal Text.
@@ -31,18 +31,18 @@ sms.sendOTP = function (phone, callback) {
  * @param callback: The method callback.
  */
 sms.sendMessage = function (phoneNumber, msg, callback) {
-    var params = {
-        Message: msg,
-        MessageStructure: 'string',
-        PhoneNumber: phoneNumber
-    };
-    sns.publish(params, function (err, data) {
-        if (err) {
-            console.log(err);
-            callback(err);
-        } else {
-            callback(false);
-        }
-    });
+   var params = {
+      Message: msg,
+      MessageStructure: 'string',
+      PhoneNumber: phoneNumber
+   };
+   sns.publish(params, function (err, data) {
+      if (err) {
+         console.log(err);
+         callback(err);
+      } else {
+         callback(false);
+      }
+   });
 };
 module.exports = sms;
--- workers.js
+++ workers.js
@@ -7,46 +7,46 @@ const workers = {};
  * Method to escalate Order status.
  */
 workers.checkOrderStatus = function () {
-    setInterval(function () {
-        workers._loopCheck();
-    }, 12 * 60 * 60 * 1000);
+   setInterval(function () {
+      workers._loopCheck();
+   }, 12 * 60 * 60 * 1000);
 };
 workers._loopCheck = function () {
-    console.log('in Workers');
-    const query = "SELECT * FROM order_details o, " +
-        "order_status_details s " +
-        "WHERE s.status='Ready-to-Pack' AND o.order_status=s.id";
-    database.query(query, function (err, pendingData) {
-        //console.log(pendingData);
-        if (err) {
-            console.log(err);
-        } else {
-            for (let i = 0; i < pendingData.length; i++) {
-                var eachOrder = pendingData[0];
-                var dateTime = eachOrder.order_date + ' 10:00:00';
-                var epochTime = moment(dateTime).valueOf();
-                //var currentTime = Math.floor((new Date().getTime()));
-                var currentTime = moment.now();
-                var difference = currentTime - epochTime;
-                var hoursDifference = Math.floor(difference / (1000 * 60 * 60));
-                var message = "Hi, order number: " + eachOrder.channel_order_id +
-                    " from " + eachOrder.channel_name + " is Ready to Pack for more than 12 Hours.";
-                let mobileNumber = "";
-                if (hoursDifference <= 13) {
-                    mobileNumber = "+918097611136";
+   console.log('in Workers');
+   const query = "SELECT * FROM order_details o, " +
+     "order_status_details s " +
+     "WHERE s.status='Ready-to-Pack' AND o.order_status=s.id";
+   database.query(query, function (err, pendingData) {
+      //console.log(pendingData);
+      if (err) {
+         console.log(err);
+      } else {
+         for (let i = 0; i < pendingData.length; i++) {
+            var eachOrder = pendingData[0];
+            var dateTime = eachOrder.order_date + ' 10:00:00';
+            var epochTime = moment(dateTime).valueOf();
+            //var currentTime = Math.floor((new Date().getTime()));
+            var currentTime = moment.now();
+            var difference = currentTime - epochTime;
+            var hoursDifference = Math.floor(difference / (1000 * 60 * 60));
+            var message = "Hi, order number: " + eachOrder.channel_order_id +
+              " from " + eachOrder.channel_name + " is Ready to Pack for more than 12 Hours.";
+            let mobileNumber = "";
+            if (hoursDifference <= 13) {
+               mobileNumber = "+918097611136";
 
-                } else {
-                    mobileNumber = "+919831140538";
-                }
-                sns.sendMessage(mobileNumber, message, function (err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Sms Send.');
-                    }
-                });
+            } else {
+               mobileNumber = "+919831140538";
             }
-        }
-    });
+            sns.sendMessage(mobileNumber, message, function (err) {
+               if (err) {
+                  console.log(err);
+               } else {
+                  console.log('Sms Send.');
+               }
+            });
+         }
+      }
+   });
 };
 module.exports = workers;
