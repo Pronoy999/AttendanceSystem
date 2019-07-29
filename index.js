0dce2dbe2c99fe43a0927ef32418877ed21af793
Writwick <writwick.das@hyperxchange.com>
2019-07-29 16:57:36 +0530

    fixed wrong function name for service issue.

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const router = {
    'franchise': handlers.franchise,
    'service-requester': handlers.serviceRequester,
    'service-request': handlers.serviceRequest,
-   'service-issue': handlers.serviceIssueStatus,
+   'service-issue': handlers.serviceIssue,
    'email': handlers.email,
    'nda': handlers.ndaEmail
 };
