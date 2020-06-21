ddc6900d4612cbc3fed07bef21762dbebb6c538c
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-06-21 18:21:30 +0530

    Update aws.js

--- aws.js
+++ aws.js
@@ -2,6 +2,6 @@ module.exports = require('aws-sdk');
 
 module.exports.config.update({
     region: 'ap-southeast-1',
-    accessKeyId: 'AKIAJIZWJEI3NUHXOC3Q',
-    secretAccessKey: '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'
+    accessKeyId: '',
+    secretAccessKey: ''
 });
